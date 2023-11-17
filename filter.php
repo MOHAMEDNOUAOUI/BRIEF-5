316a2e92c33ae689ccd904218ef4ca8f2485c4c1
MOHAMED <mohamednouaoui5@gmail.com>
2023-11-17 20:45:38 +0100

    addedall inmain

---
+++ filter.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// if(isset($_POST)) {
+//     $cnc = mysqli_connect("localhost","root","","worldcup");
+//     $request = $_POST;
+//     $query = "SELECT * FROM teams WHERE teams.IDGROUP= $request";
+//     $result = mysqli_query($cnc,$query);
+
+//    while ($row = mysqli_fetch_assoc($result)) {
+//     echo '
+//     <img src ="'.$row ["IMG"].'">
+//     ';
+//    }
+// }
