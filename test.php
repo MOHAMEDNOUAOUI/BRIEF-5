88f86aa303f92305c1b97ddea8ac608735e7c316
mohamed <mohamednouaoui5@gmail.com>
2023-11-18 22:39:10 +0100

    HERE

--- test.php
+++ test.php
@@ -24,12 +24,12 @@ $Gresult = mysqli_query($connect,$groups);
     <h1>RESULTS</h1>
     </header>
 
-    <form action="" method="POST" class="row d-flex justify-content-center my-5 gap-3 w-75 mx-auto" >
-      <input type="submit" id="ALL" value="ALL" name="ALL" class="col-md-2" >
+    <form action="" method="POST">
+      <input type="submit" value="ALL">
       <?php
         while ($rt = mysqli_fetch_assoc($Gresult))
         {?>
-        <input active class="col-md-1" type="submit" name="<?php echo $rt ["IDgroup"]?>" value="<?php echo $rt ["NAMEgroup"]?>">
+        <input type="submit" value="<?php echo $rt ["IDgroup"]?>">
         <?php
         }
       ?>
@@ -44,9 +44,9 @@ $Gresult = mysqli_query($connect,$groups);
 
 
   <?php
-  $Gresult = mysqli_query($connect, $groups);
+  
   foreach($_POST as $key => $value) {
-    if($key == 'ALL') {
+    if($key ==="ALL") {
       while ($row = mysqli_fetch_assoc($Gresult)) 
       {
         $group = $row ["IDgroup"];
@@ -90,40 +90,7 @@ while ($roww = mysqli_fetch_assoc($Tresult))
 
     }
     else {
-      $re = "SELECT * FROM teams where IDGROUP = $key";
-      $result = mysqli_query($connect,$re);
-
-      while ($row = mysqli_fetch_assoc ($result)) {
-        
-          echo '<div class="card col-md-6 px-0" style="width:16rem;" data-bs-toggle="modal" data-bs-target="#exampleModal_' . $row["IDteam"] . '">
-            <img src="'.$row["IMG"].'" alt="" class="w-75 mx-auto">
-            <h4 class="text-center">'.$row["NAMETEAM"].'</h4>
-          </div>
-          
-          <div class="modal fade" id="exampleModal_' . $row["IDteam"] . '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog">
-  <div class="modal-content">
-    <div class="modal-header d-flex flex-column align-items-center justify-content-center">
-      <img src = "'.$row ["IMG"] .'" alt = "flag" class="w-75">
-      <h3>'.$row ["NAMETEAM"].'</h3>
-    </div>
-    <div class="modal-body">
-      <p>NUMBER OF PLAYERS : '.$row ["NUMplayers"].'</p>
-      <p>CONTINENT : '.$row ["CONTINENTteam"].'</p>
-      <p>CAPITAL : '.$row ["CAPITALteam"].'</p>
-    </div>
-    <div class="modal-footer">
-      
-    </div>
-  </div>
-</div>
-</div>
-          
-          ';
-
-        
-
-      };
+      echo "HAMID";
     }
   }
 ?>
@@ -137,7 +104,7 @@ while ($roww = mysqli_fetch_assoc($Tresult))
 
 
 
-    <script src="javascript.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
